d56d68ad4ad6521fdc9362b8fa4bf457dfc575b0
Joseph Parker <jparker010190@gmail.com>
2018-03-02 03:25:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array = array.slice()
+  array = array.slice(0)
   return array
 }
 function destructivelyRemoveElementFromEndOfArray(array){
